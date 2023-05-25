84ab88757f31a2b09a4f8847cf4edd8e7186d731
Umar Farooq-Ansari <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>
2023-05-25 14:07:18 -0600

    ok

---
+++ umar.js
@@ -0,0 +1,13 @@
+function toogleHide() {
+    var btn = document.getElementById('btn');
+    var pera = document.getElementById('pera');
+if (pera.style.display != 'none'){
+    pera.style.display = 'none';
+}
+else{
+
+    pera.style.display = 'block';
+}
+
+    
+}
